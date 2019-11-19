72ffdd1dfabdaac47d01b384904d8f1c6200a48e
蒋创事 <jiangchuangshi@znlhzl.com>
2019-11-19 17:29:04 +0800

    express

--- export_test.js
+++
@@ -1,5 +0,0 @@
-const numberRegexp = /^[0-9]+$/;
-export default function (s) {
-    return s.length === 5 && numberRegexp.test(s);
-}
-//# sourceMappingURL=export_test.js.map
--- hello_ts.js
+++
@@ -1,6 +0,0 @@
-import validator from './export_test.js';
-let strings = ['Hello', '98025', '101'];
-strings.forEach(element => {
-    console.log(`"${element}" ${validator(element)}?"matches":"does not match"`);
-});
-//# sourceMappingURL=hello_ts.js.map
