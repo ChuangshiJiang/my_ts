cd8dcfc873a9c8247676931715e730e4f572f9c1
jcs <jcsweiyi@outlook.com>
2020-01-15 22:42:56 +0800

    delete and restart

--- hello_ts.ts
+++
@@ -1,7 +0,0 @@
-import validator from './export_test.js';
-
-let strings: string[] = ['Hello', '98025', '101'];
-
-strings.forEach(element => {
-  console.log(`"${element}" ${validator(element)}?"matches":"does not match"`);
-});
