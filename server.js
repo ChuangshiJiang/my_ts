8fef57f74f43259239493b8c18d375bddb097f88
蒋创事 <jiangchuangshi@znlhzl.com>
2020-03-12 19:03:19 +0800

    npm init

---
+++ server.js
@@ -0,0 +1,8 @@
+let http = require('http');
+http.createServer((request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.write('Hello world');
+  response.end();
+}).listen(8888);
