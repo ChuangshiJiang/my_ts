aa578e9402918bc386efd4d12bad036fa2b70f8f
蒋创事 <jiangchuangshi@znlhzl.com>
2020-03-13 17:58:50 +0800

    have to continue

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 let http = require('http');
-http.createServer((request, response) => {
-  response.writeHead(200, {
-    "Content-Type": "text/plain"
-  });
-  response.write('Hello world');
-  response.end();
-}).listen(8888);
+let url = require('url');
+
+function start (route,handle) {
+  function onRequest (request, response) {
+    let pathName = url.parse(request.url).pathname;
+    console.log("Request for " + pathName + " received.");
+    route(handle,pathName,response);
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('server has started');
+  
+}
+
+exports.start = start;
