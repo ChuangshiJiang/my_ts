151b49de55bbd4aa4b34a33a57fd98c2d80af111
蒋创事 <jiangchuangshi@znlh.com>
2020-04-24 23:09:03 +0800

    类

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-function greeter(person: string):string {
+function greeter(person: string): string {
     return 'hello,' + person;
 }
 
@@ -6,7 +6,7 @@ const user = 'Jane User';
 
 let value: unknown;
 
-enum Direction{
+enum Direction {
     Up,
     Down,
     Left,
@@ -16,3 +16,63 @@ enum Direction{
 console.log(Direction[0]);
 
 const a = Direction.Up;
+
+interface Say {
+    (word: string): string
+}
+
+interface User {
+    name: string
+    age?: number
+    readonly isMale: boolean
+    say: Say
+}
+
+interface VIPUser extends User{
+    broadcast: () => void
+}
+
+const getUserName = (user: User) => {
+    return user.age;
+}
+
+interface Config {
+    width?: number;
+    [propName: string]: any;
+}
+
+function CaculateAreas(config: Config): { area: number } {
+    let square = 100;
+    if (config.width) {
+        square = config.width * config.width;
+    }
+    return {
+        area: square
+    }
+}
+
+//通过类型断言，传入与接口结构不符的对象字面量
+let mySquare = CaculateAreas({ widdth: 55 } as Config);
+//
+
+//
+abstract class Animal {
+    abstract makeSound(): void;
+    protected move():void{
+        console.log('roaming the earch....');
+    }
+}
+
+class Cat extends Animal {
+    makeSound(){
+        console.log('miao miao');
+    };
+    move(){
+        console.log('movemove');
+    }
+}
+
+const animal = new Cat();
+
+animal.makeSound();
+animal.move();
