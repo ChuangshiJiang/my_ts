f7f8011081db3dfbea1345df8835c23a9fb1808d
FOCUSTECH\jiangchuangshi <18251984729@163.com>
2021-03-24 10:10:34 +0800

    use ts template

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './components/Hello';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello name="jack" enthusiasmLevel={2} />
   </React.StrictMode>,
   document.getElementById('root')
 );
